76db787818f3fc8597300c9f080065006bc0f459
Pawel Oczkowski <pawel.oczko84@gmail.com>
2019-01-10 11:01:35 +0000

    React contact

---
+++ components/Contact.js
@@ -0,0 +1,30 @@
+var Contact = React.createClass({
+    propTypes: {
+        item: React.PropTypes.object.isRequired,
+    },
+
+    render: function () {
+        return (
+            React.createElement('div', {
+                    className: 'contactItem'
+                },
+                React.createElement('img', {
+                    className: 'contactImage',
+                    src: "../user.png"
+                }),
+                React.createElement('p', {
+                    className: 'contactLabel'
+                }, 'Imię: ' + this.props.item.firstName),
+                React.createElement('p', {
+                    className: 'contactLabel'
+                }, 'Nazwisko: ' + this.props.item.lastName),
+                React.createElement('a', {
+                        className: 'contactEmail',
+                        href: 'mailto:' + this.props.item.email
+                    },
+                    this.props.item.email
+                )
+            )
+        )
+    },
+});
